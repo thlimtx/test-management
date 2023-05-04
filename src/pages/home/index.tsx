798766e03d5ef9201cce4f8270c34ac30d72f40f
Lim Tian Hee <thlim2009@hotmail.com>
2023-05-04 17:05:24 +0800

    setup: initial query and post

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,35 @@
+import { prisma } from "../../../server/db/client";
 import { Screen } from "@/components/Screen";
 
-const Home = () => {
+const Home = (props: any) => {
+  const createUser = async (item: any) => {
+    const res = await fetch("api/user", {
+      method: "POST",
+      body: JSON.stringify(item),
+    });
+  };
+
   return (
     <Screen>
       <div>Home</div>
+      <div
+        onClick={() => {
+          createUser({ name: "press3", email: "press3@press.com" });
+        }}
+      >
+        Press
+      </div>
     </Screen>
   );
+};
+
+export const getServerSideProps = async () => {
+  const allUsers = await prisma.user.findMany();
+  return {
+    props: {
+      user: allUsers,
+    },
+  };
 };
 
 export default Home;
