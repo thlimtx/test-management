b5202352bb33ac161ed0093d7b0e96f57e35d098
Lim Tian Hee <thlim2009@hotmail.com>
2023-06-03 17:20:19 +0800

    fix: screen component height

--- src/components/Screen/index.tsx
+++ src/components/Screen/index.tsx
@@ -1,6 +1,6 @@
 export const Screen = (props: any) => {
   return (
-    <div className="flex min-h-screen bg-primaryBg flex-col">
+    <div className="flex min-h-screen bg-primaryBg flex-col header-offset">
       {props.children}
     </div>
   );
