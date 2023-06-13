b824ed23967c6e47c733028c136155a5c9e3b4cb
Lim Tian Hee <thlim2009@hotmail.com>
2023-06-14 01:02:10 +0800

    fix: screen bg color

--- src/components/Screen/index.tsx
+++ src/components/Screen/index.tsx
@@ -1,6 +1,6 @@
 export const Screen = (props: any) => {
   return (
-    <div className="flex min-h-screen bg-primaryBg flex-col header-offset">
+    <div className="flex min-h-screen bg-screenBg flex-col header-offset">
       {props.children}
     </div>
   );
