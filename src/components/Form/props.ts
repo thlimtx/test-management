16e62cceb45aec32aab8880097ded08aead4d5e4
Lim Tian Hee <thlim2009@hotmail.com>
2023-06-14 15:33:24 +0800

    fix: button and form style and props

--- src/components/Form/props.ts
+++ src/components/Form/props.ts
@@ -1,12 +1,13 @@
 import { ReactNode } from "react";
 import { FieldValues, RegisterOptions } from "react-hook-form";
+import { ButtonProps } from "../Button/props";
 export type FormProps = {
   className?: string;
   title?: string;
   fields?: (React.HTMLProps<HTMLInputElement> & {
     register?: RegisterOptions<FieldValues, "">;
   })[];
-  buttons?: any;
+  buttons?: (ButtonProps & { submit?: boolean })[];
   onSubmit?: (data: any) => void;
   footer?: ReactNode;
 };
