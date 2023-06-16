bfe5b0e01014a34e9475c694560e722034a988b6
Lim Tian Hee <thlim2009@hotmail.com>
2023-06-16 14:08:37 +0800

    feat: project home page

---
+++ src/pages/project/[id]/index.tsx
@@ -0,0 +1,15 @@
+import { Screen } from "@/components/Screen";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+const Project = () => {
+  const router = useRouter();
+  useEffect(() => {
+    router && router.push(`/project/${router.query.id}/dashboard`);
+  });
+  // todo: Login session
+  const userId = 1;
+  return <Screen></Screen>;
+};
+
+export default Project;
