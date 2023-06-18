74e23f94aba13af5fa74eb82c2a7f810acc69514
Lim Tian Hee <thlim2009@hotmail.com>
2023-06-18 19:31:28 +0800

    feat: format date api

--- src/util/format.ts
+++ src/util/format.ts
@@ -1,3 +1,9 @@
+import moment from "moment";
+
 export const jsonParse = (data: any) => {
   return JSON.parse(JSON.stringify(data));
+};
+
+export const formatDate = (date: any, format?: string) => {
+  return moment(date).format(format ?? "DD/MM/YYYY");
 };
