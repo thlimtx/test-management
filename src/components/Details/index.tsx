112e1f249a53e41f7ab347a09e4f6213ddc0622a
Lim Tian Hee <thlim2009@hotmail.com>
2023-06-22 17:11:50 +0800

    fix: details component

--- src/components/Details/index.tsx
+++ src/components/Details/index.tsx
@@ -1,14 +1,14 @@
-import { DetailsProps } from "./props";
+import { DetailsProps, FieldItem } from "./props";
 import { TextInput } from "../TextInput";
 import { Button } from "../Button";
 import map from "lodash/map";
-import { useState } from "react";
 import {
   faFloppyDisk,
   faPen,
   faXmark,
 } from "@fortawesome/free-solid-svg-icons";
 import { isEmpty } from "lodash";
+import { useRouter } from "next/router";
 
 export const Details = (props: DetailsProps) => {
   const {
@@ -17,37 +17,34 @@ export const Details = (props: DetailsProps) => {
     title,
     fields,
     register,
+    onPressEdit,
     onPressSave,
     onPressCancel,
     editable,
+    isEditing,
   } = props;
 
-  const [isEditing, setIsEditing] = useState(false);
-  // todo: Login session
+  const router = useRouter();
 
-  const onPressEdit = () => {
-    setIsEditing(true);
-  };
-
-  const onCancel = () => {
-    setIsEditing(false);
-    onPressCancel && onPressCancel();
-  };
-
-  const renderDetails = (item: any) => {
+  const onPressBack = () => router.back();
+  /**
+   * Render the default component for each field
+   * @param item Each item in field props
+   */
+  const renderDetails = (item: FieldItem) => {
     const { title, register: curRegister, render, ...rest } = item;
     return (
       <div>
         <p className="text-fade text-xs my-1.5">{title}</p>
         {editable && isEditing && item.editable !== false ? (
           <TextInput
-            defaultValue={data[item.id]}
+            defaultValue={data[`${item.id}`]}
             {...rest}
             register={register}
             registerOptions={curRegister}
           />
         ) : (
-          <p>{isEmpty(data[item.id]) ? "-" : data[item.id]}</p>
+          <p>{isEmpty(data[`${item.id}`]) ? "-" : data[`${item.id}`]}</p>
         )}
       </div>
     );
@@ -66,7 +63,7 @@ export const Details = (props: DetailsProps) => {
                 icon={faXmark}
                 type="invert"
                 textClassName="text-textPrimary"
-                onPress={onCancel}
+                onPress={onPressCancel}
               />
               <Button
                 text="Save"
@@ -78,14 +75,24 @@ export const Details = (props: DetailsProps) => {
               />
             </div>
           ) : (
-            <Button
-              text="Edit"
-              className="border-textPrimary"
-              icon={faPen}
-              type="invert"
-              textClassName="text-textPrimary"
-              onPress={onPressEdit}
-            />
+            <div className="flex flex-row">
+              <Button
+                text="Back"
+                className="mr-3 border-textPrimary"
+                icon={faPen}
+                type="invert"
+                textClassName="text-textPrimary"
+                onPress={onPressBack}
+              />
+              <Button
+                text="Edit"
+                className="border-textPrimary"
+                icon={faPen}
+                type="invert"
+                textClassName="text-textPrimary"
+                onPress={onPressEdit}
+              />
+            </div>
           ))}
       </div>
       <div className="p-4 my-2 bg-primaryBg shadow">
--- src/components/Details/props.ts
+++ src/components/Details/props.ts
@@ -1,20 +1,73 @@
 import { ReactNode } from "react";
 import { FieldValues, RegisterOptions, UseFormRegister } from "react-hook-form";
+
+export type FieldItem = React.HTMLProps<HTMLInputElement> & {
+  /**
+   * Register options for useForm register of each field, register("id", registerOptions);
+   */
+  register?: RegisterOptions<FieldValues, "">;
+  /**
+   * Allows for custom fields
+   * @param data
+   * @returns
+   */
+  render?: (data: {
+    /**
+     * Data from data props
+     */
+    data?: any;
+    isEditing?: boolean;
+    /**
+     * Render the default component for each field
+     * @param item Each item in field props
+     */
+    renderDetails: (item: FieldItem) => ReactNode;
+  }) => any;
+  /**
+   * Whether the field is editable
+   */
+  editable?: boolean;
+};
+
 export type DetailsProps = {
+  /**
+   * Data of different fields to show
+   * - data will show in fields based on data key -> field id
+   */
   data?: any;
   className?: string;
   title?: string;
-  fields?: (React.HTMLProps<HTMLInputElement> & {
-    register?: RegisterOptions<FieldValues, "">;
-    render?: (data: {
-      data: any;
-      isEditing: boolean;
-      renderDetails: (item: any) => ReactNode;
-    }) => any;
-    editable?: boolean;
-  })[];
+  /**
+   * Each field of details and input fields when edit is on
+   * - contains html input props
+   * - default value can be determined by data props
+   */
+  fields?: FieldItem[];
+  /**
+   * On press edit
+   * @returns void
+   */
+  onPressEdit?: () => void;
+  /**
+   * On cancel edit
+   * @returns void
+   */
   onPressCancel?: () => void;
+  /**
+   * On save after edit mode
+   * @returns void
+   */
   onPressSave?: () => void;
+  /**
+   * useForm register function
+   */
   register?: UseFormRegister<FieldValues>;
+  /**
+   * Whether to allow showing edit option
+   */
   editable?: boolean;
+  /**
+   * Toggle edit externally
+   */
+  isEditing?: boolean;
 };
