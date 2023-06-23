0f38854ef9df6ae858dca6ca05d6ed8f0f8fd710
Lim Tian Hee <thlim2009@hotmail.com>
2023-06-23 12:40:38 +0800

    feat: project details screen

--- src/pages/api/project/update.ts
+++ src/pages/api/project/update.ts
@@ -2,10 +2,10 @@ import { prisma } from "server/db/client";
 
 const handler = async (req: any, res: any) => {
   const { body } = req;
-  const { id, name } = JSON.parse(body);
+  const { id, ...rest } = JSON.parse(body);
   const user = await prisma.project.update({
     where: { id },
-    data: { name },
+    data: { ...rest },
   });
   return res.status(200).json(user);
 };
---
+++ src/pages/project/[id]/details/data.tsx
@@ -0,0 +1,27 @@
+export const projectFields = [
+  {
+    id: "name",
+    title: "Name",
+    placeholder: "Enter Name",
+  },
+  {
+    id: "description",
+    title: "Description",
+    placeholder: "Enter Description",
+  },
+  {
+    id: "version",
+    title: "Version",
+    placeholder: "Enter Version",
+  },
+  {
+    id: "env",
+    title: "Environment",
+    placeholder: "Enter Environment",
+  },
+  {
+    id: "tools",
+    title: "Tools",
+    placeholder: "Enter Tools",
+  },
+];
