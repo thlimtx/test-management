bafcd9c95315c07c1943e916a534a62de8f49326
Lim Tian Hee <thlim2009@hotmail.com>
2023-06-28 12:10:08 +0800

    feat: project title header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,31 @@
 import { useRouter } from "next/router";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faAppleWhole } from "@fortawesome/free-solid-svg-icons";
 import { Button } from "../Button";
+import { useEffect, useState } from "react";
+import { Project } from "@prisma/client";
 
 export const Header = (props: any) => {
   const router = useRouter();
+  const projectId = router.query.id;
+
+  const [project, setProject] = useState<Project | null>();
+
+  useEffect(() => {
+    findProject(projectId);
+  }, [projectId]);
+
+  const findProject = async (item: any) => {
+    const res = await fetch("/api/project/find", {
+      method: "POST",
+      body: JSON.stringify({ id: parseInt(item) }),
+    });
+    if (res.ok) {
+      const data = await res.json();
+      setProject(data);
+    } else {
+      setProject(null);
+    }
+  };
+
   return (
     <div
       className={`fixed header-height mb-1 w-full shadow bg-primaryBg p-2 flex flex-row items-center ${props.className}`}
@@ -13,18 +34,25 @@ export const Header = (props: any) => {
       <div className="flex-1 flex flex-row px-2">
         <img src="https://picsum.photos/30" />
       </div>
-      {/* <FontAwesomeIcon icon={faAppleWhole} size="sm" /> */}
-      <Button
-        text="Sign up"
-        className="mr-2 rounded-full"
-        type="invert"
-        onPress={() => router.push("/signup")}
-      />
-      <Button
-        text="Login"
-        className="rounded-full"
-        onPress={() => router.push("/login")}
-      />
+      {projectId ? (
+        <div className="flex flex-row mx-5">
+          <p>{project?.name}</p>
+        </div>
+      ) : (
+        <div className="flex flex-row">
+          <Button
+            text="Sign up"
+            className="mr-2 rounded-full"
+            type="invert"
+            onPress={() => router.push("/signup")}
+          />
+          <Button
+            text="Login"
+            className="rounded-full"
+            onPress={() => router.push("/login")}
+          />
+        </div>
+      )}
     </div>
   );
 };
