94cd74ec8f14e409ab9c41533cf188afc6934e49
Lim Tian Hee <thlim2009@hotmail.com>
2023-06-28 12:29:21 +0800

    feat: omit option for dropdown option util

--- src/util/data.ts
+++ src/util/data.ts
@@ -1,15 +1,18 @@
-import { capitalize, map } from "lodash";
+import { capitalize, filter, includes, map } from "lodash";
 
 /**
  * Get dropdown option data type from enum
  * @param type enum type
  * @returns array of object with key and label attribute
  */
-export const getDropdownOptionsbyType = (type: any) => {
-  return map(type, (o) => {
-    return {
-      key: o,
-      label: capitalize(o),
-    };
-  });
+export const getDropdownOptionsbyType = (type: any, omit?: string[]) => {
+  return map(
+    filter(type, (o) => !includes(o, omit)),
+    (o) => {
+      return {
+        key: o,
+        label: capitalize(o),
+      };
+    }
+  );
 };
